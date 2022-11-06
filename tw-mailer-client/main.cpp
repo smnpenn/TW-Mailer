3c13a706c934ba83e700933e468dbbd17672976e
Simon <smnpenn@gmail.com>
2022-11-06 14:28:30 +0100

    Server now gets user and creates folder for user

--- tw-mailer-client/main.cpp
+++ tw-mailer-client/main.cpp
@@ -21,31 +21,38 @@ vector<string> splitInputMessage(const string& string){
     return tokens;
 }
 
+void sendMessageToServer(int sock, string message){
+        if(send(sock, message.data(), message.size(), 0) < 0){
+            perror("Server error");
+            //return errno;
+        }
+        cout << "Sent message" << endl;
+}
+
 
 int main(int argc, char *argv[])
 {
+    if(argc<3){
+        cout << "Invalid number of arguments\nUsage: ./filename <server_ip> <port>" << endl;
+        return -1;
+    }
+
     char username[8];
-    int n = 0;
+    //int n = 0;
+    char* server_ip = argv[1];
+    int server_port = atoi(argv[2]);
 
     // Usage ./twmailer-client <ip> <port>
     
-    cout << "Hello " << argv[1] << " " << argv[2]<< endl;
+    cout << "Chosen ip adrress and port: " << server_ip << " : " << server_port << endl;
     
-        
-    
-    cout << "Chosen ip adrress and port: " << argv[1] << " : " << argv[2] << endl;
-    
-    
-    cout << "Enter username " << endl;
+    cout << "Enter username: " << endl;
     
     cin >> username;
     
     cout << "Chosen username " << username << endl;
     
     // ---------- SOCKET CREATION -----------
-
-
-
     
     int sock = 0, client_fd;
     
@@ -57,9 +64,9 @@ int main(int argc, char *argv[])
     }
     
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_port = htons(server_port);
     
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0){
+    if(inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0){
         cout << "Address invalid/not supported" << endl;
         return -1;
     }
@@ -69,6 +76,11 @@ int main(int argc, char *argv[])
         printf("\nConnection Failed \n");
         return -1;
     }
+    cout << "Connected" << endl;
+
+    //send username
+    string username_str;
+    sendMessageToServer(sock, username_str.assign(username, username+8));
 
     while(true){
         vector<string> messageTokens;
@@ -77,7 +89,7 @@ int main(int argc, char *argv[])
 
         getline(cin, message, '.');
 
-        messageTokens = splitInputMessage(message);
+        /*messageTokens = splitInputMessage(message);
 
         if(messageTokens[1] == "SEND"){ // maybe um die Modes zu checken -> wird wsh eh nicht gebraucht.
             cout << "MODE: SENDING" << endl;
@@ -86,18 +98,11 @@ int main(int argc, char *argv[])
         } else if(messageTokens[1] == "QUIT"){
             cout << "QUITTING" << endl;
             break;
-        }
+        }*/
 
-        n = (send(sock, message.data(), message.size(), 0)) < 0;
-        if(n){
-            perror("Server error");
-            return errno;
-        }
-        cout << "Sent message" << endl;
+        sendMessageToServer(sock, message);
 
         message.clear();
-
-
     }
 
 
--- tw-mailer-server/main.cpp
+++ tw-mailer-server/main.cpp
@@ -7,6 +7,12 @@
 #include <algorithm>
 #include <vector>
 #include <sstream>
+#include <unistd.h>
+#include <errno.h>
+#include <filesystem>
+#include <fstream>
+
+#define MAXLINE 1500
 #define PORT 8080
 
 using namespace std;
@@ -21,21 +27,79 @@ vector<string> splitInputMessage(const string& string){
     return tokens;
 }
 
+// static ssize_t
+// my_read (int fd, char *ptr)
+// {
+//     static int read_cnt = 0 ;
+//     static char *read_ptr ;
+//     static char read_buf[MAXLINE] ;
+//     if (read_cnt <= 0) {
+//         again:
+//         if ( (read_cnt = read(fd,read_buf,sizeof(read_buf))) < 0) {
+//             if (errno == EINTR)
+//                 goto again ;
+//             return (-1);
+//         } else if (read_cnt == 0)
+//             return (0);
+//         read_ptr = read_buf;
+//     };
+
+//     read_cnt--;
+//     *ptr = *read_ptr++;
+//     return (1);
+// }
+// ssize_t readline (int fd, void *vptr, size_t maxlen)
+// {
+//     ssize_t n, rc;
+//     char c, *ptr;
+//     ptr = vptr;
+//     for (n = 1 ; n < maxlen ; n++) {
+//         if ( (rc = my_read(fd,&c)) == 1 ) {
+//             *ptr++ = c;
+//             if (c == '\n')
+//                 break ; // newline is stored
+//         } else if (rc == 0) {
+//             if (n == 1)
+//                 return (0); // EOF, no data read
+//             else
+//                 break; // EOF, some data was read
+//         } else
+//             return (-1); // error, errno set by read() in my_read()
+//     };
+//     *ptr = 0; // null terminate
+//     return (n);
+// } 
+
+
+void messageToFile(string message, filesystem::path path){
+    fstream myFile(path / "current_id.txt");
+    myFile << "1";
+
+}
+
 int main(int argc, char *argv[])
 {
+    if(argc<3){
+        cout << "Invalid number of arguments\nUsage: ./filename <port> <mail-spool-directory>" << endl;
+        return -1;
+    }
+
     int server_fd, new_socket;
-
     struct sockaddr_in address;
-
     char buffer[1024];
-
     vector<string> messageTokens;
-
     string message;
-
     int opt = 1;
-
     int n = 0;
+    int port = atoi(argv[1]);
+    string mailspool_dir = argv[2];
+    filesystem::path mailspool_path = filesystem::current_path() / mailspool_dir;
+
+    cout << "Path: " << mailspool_path << endl;
+
+    if(!filesystem::exists(mailspool_path)){
+        filesystem::create_directory(mailspool_path);
+    }
 
     int addrlen = sizeof(address);
 
@@ -51,7 +115,7 @@ int main(int argc, char *argv[])
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(port);
 
     if(bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0){
         perror("bind failed");
@@ -61,23 +125,44 @@ int main(int argc, char *argv[])
         perror("listen");
         exit(EXIT_FAILURE);
     }
+
+    cout << "Server running and listening on port " << port << endl;
+
     if ((new_socket = accept(server_fd, (struct sockaddr*)&address,(socklen_t*)&addrlen))< 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
 
+    string username;
+    
+    //get username
+    if((n=recv(new_socket, buffer, sizeof(buffer), 0))>0){
+        username.append(buffer, buffer+n);
+    }
+
+    cout << username << " connected!" << endl;
+    filesystem::path mailspool_userDir = mailspool_path / username;
+
+    if(!filesystem::exists(mailspool_userDir)){
+        filesystem::create_directory(mailspool_userDir);
+        
+    }
+
     // Function to read incoming message
     while((n=recv(new_socket, buffer, sizeof(buffer), 0))>0){
         message.append(buffer, buffer+n);
-
+        messageToFile(message, mailspool_userDir);
         messageTokens = splitInputMessage(message);
 
         for(auto const &tokens : messageTokens){
             cout << tokens << endl;
         }
+
+        //messageToFile(message, mailspool_userDir);
         message.clear();
         messageTokens.clear();
     }
 
     return 0;
 }
+
